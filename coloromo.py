5eb5693ae8763da14eb270e7d391d35f75c81b4f
William Lee <wlee753159@gmail.com>
2021-02-18 11:12:54 +0000

    Add Coloromo class

--- coloromo.py
+++ coloromo.py
@@ -25,3 +25,13 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+
+
+class Coloromo:
+    """
+    A class for performing image palette reductions.
+    """
+
+    def __init__(self):
+        self.palette = []
+        self.cache = {}
